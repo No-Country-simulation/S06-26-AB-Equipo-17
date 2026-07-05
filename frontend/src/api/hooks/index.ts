d077fe48664b95866f37c2c3753fd06d2a760046
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-07-04 21:57:14 -0300

    feat: add api hooks

--- frontend/src/api/hooks/index.ts
+++ frontend/src/api/hooks/index.ts
@@ -1,4 +1,6 @@
 export { useApi } from "./useApi";
 export type { AsyncState } from "./useApi";
-export { useMapData } from "./useMapData";
+export { useMapNetwork } from "./useMapNetwork";
+export { useMapMobility } from "./useMapMobility";
+export { useMapOverview } from "./useMapOverview";
 export { useAIQuery } from "./useAIQuery";
